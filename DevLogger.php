a535d54483aec64634a38618d8bf7f5f0a202649
Glomberg <bazz@bk.ru>
2026-05-19 15:36:39 +0300

    Fix. Logger. Check `error_log` function.

--- DevLogger.php
+++ DevLogger.php
@@ -20,6 +20,9 @@ class DevLogger
         if ( ! is_string($msg) ) {
             return;
         }
+        if ( ! function_exists('error_log') ) {
+            return;
+        }
         /**
          * @psalm-suppress ForbiddenCode
          */
